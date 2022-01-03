a3448e0905ea4e3ed5cab21b146590df734b02fa
LAPTOP-M8OJ4LN0\Juanraden <juanraden@gmail.com>
2022-01-03 22:08:21 +0700

    Problem 4 v1.1

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -38,34 +38,35 @@ public class BallControl : MonoBehaviour
 
     private void ArrowMovement()
     {
-        if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && direction.x > Vector2.left.x)
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
             direction += Vector2.left;
 
-            if (direction == Vector2.zero)
+            if (direction == Vector2.zero || direction.x < Vector2.left.x)
                 direction = Vector2.left;
         }
 
-        if ((Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) && direction.y > Vector2.down.y)
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
             direction += Vector2.down;
 
-            if (direction == Vector2.zero)
+            if (direction == Vector2.zero || direction.y < Vector2.down.y)
                 direction = Vector2.down;
         }
 
-        if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) && direction.x < Vector2.right.x)
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
             direction += Vector2.right;
 
-            if (direction == Vector2.zero)
+            if (direction == Vector2.zero || direction.x > Vector2.right.x)
                 direction = Vector2.right;
         }
 
-        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && direction.y < Vector2.up.y)
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
             direction += Vector2.up;
-            if (direction == Vector2.zero)
+
+            if (direction == Vector2.zero || direction.y > Vector2.up.y)
                 direction = Vector2.up;
         }
     }
