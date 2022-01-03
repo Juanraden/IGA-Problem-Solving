f3d218204b15378552db920dc86a1d88d5697b94
LAPTOP-M8OJ4LN0\Juanraden <juanraden@gmail.com>
2022-01-03 16:13:33 +0700

    Problem 2 v1.0

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    private Vector2 direction;
+
+    public float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+        direction = Random.insideUnitCircle.normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveBall();
+    }
+
+    void MoveBall()
+    {
+        rigidBody2D.velocity = direction * speed;
+    }
+}
